0b7f23c1aed142681f1029cc501d642e2bf9ee83
Zane Armstrong <zane.armstrong@comcast.net>
2020-10-15 10:47:29 -0500

    fix bg video

--- src/Interface/Components/Welcome.js
+++ src/Interface/Components/Welcome.js
@@ -3,7 +3,7 @@ import { Route, BrowserRouter as Router, Link } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 
 import BackgroundVideo from "../../Videos/Components/BackgroundVideo";
-import welcomeVideo from "../../Videos/trashwav-axel-fix.mp4";
+import welcomeVideo from "../../Videos/trashwav-axel.mp4";
 import logo from "../../Interface/Images/trashwavlogo.png";
 
 const Routes = React.lazy(() => import("./Routes"));
