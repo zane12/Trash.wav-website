eac9a990c49ae2bcbcca03e4c04de9a14a658242
Zane Armstrong <zane.armstrong@comcast.net>
2020-10-20 12:07:03 -0500

    changed news content

--- src/News/Components/News.js
+++ src/News/Components/News.js
@@ -24,33 +24,12 @@ export default function News(props) {
           <h1 className="news">No news!!!</h1>
           <p className="news">
             However, if there were some news, it would look something like this.
-            Whatever sort of length it has to it we need to make sure and check
-            that it wraps correctly and automatically formats nicely so that
-            it's easy to add news later if this website is ever going to get
-            used. Really for now its a test project so I just need some text
-            here to test my CSS and whatnot.
+            I have some music pretty much ready for release, but I have to remaster it and make some sort of media to go along with it.
+            If you see this and wanna help me get it released, email me at trash.wav33@gmail.com.
           </p>
         </div>
       </CSSTransition>
-      <CSSTransition
-        key="news2"
-        in={props.active && active}
-        classNames="news"
-        timeout={2000}
-      >
-        <div className="news news-container">
-          <h1 className="news">No news!!!</h1>
-          <p className="news">
-            However, if there were some news, it would look something like this.
-            Whatever sort of length it has to it we need to make sure and check
-            that it wraps correctly and automatically formats nicely so that
-            it's easy to add news later if this website is ever going to get
-            used. Really for now its a test project so I just need some text
-            here to test my CSS and whatnot.
-          </p>
-          
-        </div>
-      </CSSTransition>
+      
       <BackButton onLoad={()=>setActive(true)} active={props.active}/>
       <Footer active={footer} />
       
